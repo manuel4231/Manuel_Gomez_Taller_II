326d560813ba186d704dcc3277d9ec57c8016d09
manuel4231 <manuel123457@gmail.com>
2018-10-22 01:40:57 -0500

    por FIn

---
+++ Juego Infeccion/Assets/SCRIPTS/ObjetoGanador.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetoGanador : MonoBehaviour {
+
+    public GameObject objeto_ganador;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            objeto_ganador.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Juego Infeccion/Assets/SCRIPTS/Vacuna.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuna : MonoBehaviour {
+
+    public int enfermedad_cura;
+
+    public float cantidad_inmunitaria;
+
+}
